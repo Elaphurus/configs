32a477a48f3fea501865783d2ee67b8519bb2a7e
Mingzhe Hu <humingzhework@163.com>
2022-08-03 16:28:29 +0800

    Update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,12 +3,12 @@
 ;; 关闭启动帮助画面
 (setq inhibit-splash-screen 1)
 ;; 显示空白字符
-(global-whitespace-mode 1)
+;(global-whitespace-mode 1)
 ;; 启动窗口大小
 (setq default-frame-alist
 '((height . 45) (width . 85)))
 ;; 设置字号15pt
-(set-face-attribute 'default nil :height 150)
+(set-face-attribute 'default nil :height 145)
 ;; 关闭工具栏
 (tool-bar-mode -1)
 ;; 关闭滑动条
@@ -23,10 +23,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(company)))
+ '(package-selected-packages '(markdown-mode company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(defun open-markdown-preview ()
+  (interactive)
+  (browse-url "http://localhost:6419"))
+(global-set-key (kbd "C-c m") 'open-markdown-preview)
